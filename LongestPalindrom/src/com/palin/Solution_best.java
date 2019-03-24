0d1b95d16a56cdcb56c655c93eb4b5a80a2635c2
prashant <seudo.pro@gmail.com>
2019-03-24 16:31:01 -0400

    Update Solution_best.java

--- LongestPalindrom/src/com/palin/Solution_best.java
+++ LongestPalindrom/src/com/palin/Solution_best.java
@@ -1,31 +1,32 @@
 package com.palin;
 
 public class Solution {
-	public String longestPalindrome(String s) {
-	    int start = 0, end = 0;
-	    for (int i = 0; i < s.length(); i++) {
-	        int len1 = expandAroundCenter(s, i, i);
-	        int len2 = expandAroundCenter(s, i, i + 1);
-	        int len = Math.max(len1, len2);
-	        if (len > end - start) {
-	            start = i - (len - 1) / 2;
-	            end = i + len / 2;
-	        }
-	    }
-	    return s.substring(start, end + 1);
-	}
+private int lo, maxLen;
 
-	private int expandAroundCenter(String s, int left, int right) {
-	    int L = left, R = right;
-	    while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
-	        L--;
-	        R++;
-	    }
-	    return R - L - 1;
+public String longestPalindrome(String s) {
+	int len = s.length();
+	if (len < 2)
+		return s;
+	
+    for (int i = 0; i < len-1; i++) {
+     	extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
+     	extendPalindrome(s, i, i+1); //assume even length.
+    }
+    return s.substring(lo, lo + maxLen);
+}
+
+private void extendPalindrome(String s, int j, int k) {
+	while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+		j--;
+		k++;
 	}
 	
-	public static void mains(String[] args) {
-		Solution sol = new Solution();
-		System.out.println(sol.longestPalindrome("babad"));
+/*
+When we come out from the loop, the character of index j and k should no more be part of the palindrome, 
+i.e, j+1, k-1 are the first and the last character of the palindrome respectively. maxlen = (k-1)-(j+1)+1 = k - j - 1.
+*/
+	if (maxLen < k - j - 1) {
+		lo = j + 1;
+		maxLen = k - j - 1;
 	}
-}
+}}
