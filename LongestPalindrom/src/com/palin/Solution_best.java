3abb6d21a9cfddc8420095fac048f167820e98e1
prashant <seudo.pro@gmail.com>
2019-03-24 20:14:32 -0400

    Update Solution_best.java

--- LongestPalindrom/src/com/palin/Solution_best.java
+++ LongestPalindrom/src/com/palin/Solution_best.java
@@ -1,38 +1,44 @@
 /*
 https://leetcode.com/problems/longest-palindromic-substring/discuss/2928/Very-simple-clean-java-solution
 */
-package com.palin;
+package longestpalindrome;
 
 public class Solution {
-private int lo, maxLen;
+	private int lo, maxLen;
 
-public String longestPalindrome(String s) {
-	int len = s.length();
-	if (len < 2)
-		return s;
-	
-    for (int i = 0; i < len-1; i++) {
-     	extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
-     	extendPalindrome(s, i, i+1); //assume even length.
-    }
-    return s.substring(lo, lo + maxLen);
-}
+	public String longestPalindrome(String s) {
+		int len = s.length();
+		if (len < 2)
+			return s;
 
-private void extendPalindrome(String s, int j, int k) {
-	while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
-		j--;
-		k++;
+		for (int i = 0; i < len - 1; i++) {
+			extendPalindrome(s, i, i);      // assume odd length, try to extend Palindrome as possible
+			extendPalindrome(s, i, i + 1); //  assume even length.
+		}
+		return s.substring(lo, lo + maxLen);
 	}
-	
+
+	private void extendPalindrome(String s, int j, int k) {
+		while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+			j--;
+			k++;
+		}
 /*
 When we come out from the loop, the character of index j and k should no more be part of the palindrome, 
 i.e, j+1, k-1 are the first and the last character of the palindrome respectively. maxlen = (k-1)-(j+1)+1 = k - j - 1.
 */
-	if (maxLen < k - j - 1) {
-		lo = j + 1;
-		maxLen = k - j - 1;
+		if (maxLen < k - j - 1) {
+			lo = j + 1;
+			maxLen = k - j - 1;
+		}
 	}
-}}
+	
+	public static void main(String[] args) {
+		String s = "abaxxx";
+		Solution sol = new Solution();
+		System.out.println(sol.longestPalindrome(s));
+	}
+}
 
 /*
 package longestpalindrome;
