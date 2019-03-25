4af4ee5b763dd0bb527e6bd1497992530ebf860d
prashant <seudo.pro@gmail.com>
2019-03-24 23:19:55 -0400

    Update Solution.java

--- threesum/Solution.java
+++ threesum/Solution.java
@@ -1,27 +1,33 @@
+package threesum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-    Arrays.sort(nums);
-    for (int i = 0; i + 2 < nums.length; i++) {
-        if (i > 0 && nums[i] == nums[i - 1]) {              // skip same result
-            continue;
-        }
-        int j = i + 1, k = nums.length - 1;  
-        int target = -nums[i];
-        while (j < k) {
-            if (nums[j] + nums[k] == target) {
-                res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                j++;
-                k--;
-                while (j < k && nums[j] == nums[j - 1]) j++;  // skip same result
-                while (j < k && nums[k] == nums[k + 1]) k--;  // skip same result
-            } else if (nums[j] + nums[k] > target) {
-                k--;
-            } else {
-                j++;
-            }
-        }
-    }
-    return res;
-    }
+	public List<List<Integer>> threeSum(int[] nums) {
+		List<List<Integer>> res = new ArrayList<>();
+		Arrays.sort(nums);
+		for (int i = 0; i + 2 < nums.length; i++) {
+			int j = i + 1, k = nums.length - 1;
+			int target = -nums[i];
+			while (j < k) {
+				if (nums[j] + nums[k] == target) {
+					res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+					j++;
+					k--;
+				} else if (nums[j] + nums[k] > target) {
+					k--;
+				} else {
+					j++;
+				}
+			}
+		}
+		return res;
+	}
+	
+	public static void main(String[] args) {
+		int[] nums = {-1, 0, 1, 2, -1, -4};
+		System.out.println(new Solution().threeSum(nums));
+	}
 }
