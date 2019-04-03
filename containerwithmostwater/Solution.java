b52f637d036bde7bd9b45f33955369ae7750a689
prashant <seudo.pro@gmail.com>
2019-04-03 19:24:40 -0400

    Update Solution.java

--- containerwithmostwater/Solution.java
+++ containerwithmostwater/Solution.java
@@ -1,5 +1,32 @@
 package containerwithmostwater;
 
+/*
+ * https://leetcode.com/problems/container-with-most-water/discuss/6099/Yet-another-way-to-see-what-happens-in-the-O(n)-algorithm
+ */
+
+class Solution {
+    public int maxArea(int[] h) {
+        int area = 0, i = 0, j = h.length - 1;
+        
+        while (i < j) {
+            int width = j - i;
+            int height = h[i] <= h[j] ? h[i++] : h[j--];
+            int currArea = width * height;
+            
+            if (currArea > area)
+                area = currArea; 
+        }
+        
+        return area;
+    }
+    
+    public static void main(String[] args) {
+		int[] heights = {1,8,6,2,5,4,8,3,7};
+		System.out.println(new Solution().maxArea(heights));
+	}
+}
+
+/*
 public class Solution {
 
 	public static int maxArea(int[] height) {
@@ -19,7 +46,8 @@ public class Solution {
 	
 	public static void main(String[] args) {
 		int[] arr = {1,8,6,2,5,4,8,3,7};
-		/*int[] arr = {1,2,1};*/
+		/*int[] arr = {1,2,1};
 		System.out.println(maxArea(arr));
 	}
 }
+*/
