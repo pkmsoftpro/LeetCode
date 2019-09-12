64b2ff634a76152c41efa13421be84a2dc377776
prashant <pkmprofessional@gmail.com>
2019-09-11 23:55:54 -0400

    Update Solution_JeanTimex.java

--- LongestPalindrom/src/com/palin/Solution_JeanTimex.java
+++ LongestPalindrom/src/com/palin/Solution_JeanTimex.java
@@ -5,6 +5,7 @@
  * Time complexity O(n^2).
  * Space complexity O(n^2).
  *
+ * https://leetcode.com/problems/longest-palindromic-substring/discuss/2921/Share-my-Java-solution-using-dynamic-programming
  */
 
 
