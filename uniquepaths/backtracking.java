c9141af8890b3e48e1bd02b4f9772711b509004b
prashant <pkmprofessional@gmail.com>
2019-09-14 14:38:02 -0400

    Update backtracking.java

--- uniquepaths/backtracking.java
+++ uniquepaths/backtracking.java
@@ -1,4 +1,19 @@
 package uniquepaths;
+/**
+ * https://leetcode.com/problems/unique-paths/discuss/374561/100-or-5-Solutions-or-Step-by-Step-optimisation-or-Dp-thought-process
+ *
+ * We can solve this problem through backtracking.
+ * Backtracking
+ * 1. Our goal: To reach destination cell (m-1,n-1), once reach it counted as one path
+ * 2. Our constraints: a) We can not move outside of boundary b
+ * 3. Our choices: a) we can move either Down (i+1,j) or right (i, j+1) from any cell
+ * <p>
+ * Complexity:
+ * To reach a cell there are two way and we try all the source cell to reach this cell.
+ * Complexity: O((m*n)^2) /O (m*n)
+ * <p>
+ * TLE
+ */
 
 public class backtracking {
 
