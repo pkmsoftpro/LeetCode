e974b4c5c1293c83ffe87effc3d05413eb1f6d23
prashant <prashant.kr.mod@gmail.com>
2020-07-13 18:10:39 -0400

    Create Solution.java

---
+++ DisjointSet/Solution.java
@@ -0,0 +1,53 @@
+package redundantConnection_684;
+
+public class Solution {
+  
+  public static void main(String[] args) {
+    int[][] edges = {{1,2}, {2,3}, {3,4}, {1,4}, {1,5}};
+    int[] x = new Solution().findRedundantConnection(edges);
+    System.out.println(x[0]+":"+x[1]);
+  }
+  public int[] findRedundantConnection(int[][] edges) {
+    DisjointSet disjointSet = new DisjointSet(edges.length);
+    
+    for (int[] edge : edges) {
+        if (!disjointSet.union(edge[0] - 1, edge[1] - 1)) return edge;
+    }
+    
+    throw new IllegalArgumentException();
+}
+
+static class DisjointSet {
+    
+    private int[] parent;
+    private byte[] rank;
+    
+    public DisjointSet(int n) {
+        if (n < 0) throw new IllegalArgumentException();
+        parent = new int[n];
+        rank = new byte[n];
+    }
+    
+    public int find(int x) {
+        if (parent[x] == 0) return x;
+        return parent[x] = find(parent[x]); // Path compression by halving.
+    }
+    
+    // Return false if x, y are connected.
+    public boolean union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX == rootY) return false;
+        
+        // Make root of smaller rank point to root of larger rank.
+        if (rank[rootX] < rank[rootY]) parent[rootX] = rootY;
+        else if (rank[rootX] > rank[rootY]) parent[rootY] = rootX;
+        else {
+            parent[rootX] = rootY;
+            rank[rootY]++;
+        }
+        
+        return true;
+    }
+}
+}
