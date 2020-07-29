a6e1869c3f47e6f8bd073247841ce9de77f87648
prashant <prashant.kr.mod@gmail.com>
2020-07-28 23:42:25 -0400

    Update Sol.java

--- graph/safestates_802/Sol.java
+++ graph/safestates_802/Sol.java
@@ -1,4 +1,9 @@
 /*
+
+Method 1: Topological Sort
+Using degree array to record the out-degree, neighbors map to record In-neighbors(for example 0->1, 2->1, map(1) = [0, 2]).
+Add the node whose out-degree is 0 into queue and result Set, then process each node in the queue, if the out-degree of one node becomes 0, add it to queue until queue is empty.
+
 https://leetcode.com/problems/find-eventual-safe-states/discuss/120633/Java-Solution-(DFS-andand-Topological-Sort)
 */
 
