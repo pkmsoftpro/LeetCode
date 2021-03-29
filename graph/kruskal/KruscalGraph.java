3ed7a86eeafadf0d5df8bfaf2b49d0d96655d521
prashant <prashant.kr.mod@gmail.com>
2021-03-28 21:36:46 -0400

    prims algorithm gfg

---
+++ graph/kruskal/KruscalGraph.java
@@ -0,0 +1,15 @@
+package kruskal;
+
+public class KruscalGraph {
+
+  public int V, E;
+
+  public KruscalGraph(int v, int e){
+    this.V = v;
+    this.E = e;
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
