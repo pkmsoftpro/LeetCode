0b5dc57c52b9e1eba2b76d100742364933a5ac37
prashant <prashant.kr.mod@gmail.com>
2021-04-10 11:02:47 -0400

    Update Solution.java

--- threesum_15/Solution.java
+++ threesum_15/Solution.java
@@ -6,25 +6,30 @@ import java.util.List;
 
 class Solution {
 	public List<List<Integer>> threeSum(int[] nums) {
-		List<List<Integer>> res = new ArrayList<>();
-		Arrays.sort(nums);
-		for (int i = 0; i + 2 < nums.length; i++) {
-			int j = i + 1, k = nums.length - 1;
-			int target = -nums[i];
-			while (j < k) {
-				if (nums[j] + nums[k] == target) {
-					res.add(Arrays.asList(nums[i], nums[j], nums[k])); // you can skip duplicates here
-					j++;
-					k--;
-				} else if (nums[j] + nums[k] > target) {
-					k--;
-				} else {
-					j++;
-				}
-			}
+            List<List<Integer>> res = new ArrayList<>();
+	    Arrays.sort(nums);
+	    for (int i = 0; i + 2 < nums.length; i++) {
+		if (i > 0 && nums[i] == nums[i - 1]) {              // skip same result
+		    continue;
 		}
-		return res;
-	}
+		int j = i + 1, k = nums.length - 1;  
+		int target = -nums[i];
+		while (j < k) {
+		    if (nums[j] + nums[k] == target) {
+			res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+			j++;
+			k--;
+			while (j < k && nums[j] == nums[j - 1]) j++;  // skip same result
+			while (j < k && nums[k] == nums[k + 1]) k--;  // skip same result
+		    } else if (nums[j] + nums[k] > target) {
+			k--;
+		    } else {
+			j++;
+		    }
+		}
+    	    }
+    		return res;
+    	}
 	
 	public static void main(String[] args) {
 		int[] nums = {-1, 0, 1, 2, -1, -4};
