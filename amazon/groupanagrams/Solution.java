c953391b549f9cb86e31c8c4ccd037ee0a57f5a9
prashant <prashant.kr.mod@gmail.com>
2021-04-16 22:43:07 -0400

    groupanagrams

--- amazon/groupanagrams/Solution.java
+++ amazon/groupanagrams/Solution.java
@@ -1,2 +1,23 @@
+package groupanagrams;
+
+import java.util.*;
+
 public class Solution {
+  public List<List<String>> groupAnagrams(String[] strs) {
+    Map<String, List<String>> map = new HashMap<>();
+    for(String str : strs){
+      char[] c = str.toCharArray();
+      Arrays.sort(c);
+      String key = new String(c);
+      if(map.containsKey(key)){
+        map.get(key).add(str);
+      } else map.put(key, new ArrayList<>(){{add(str);}});
+    }
+    return new ArrayList(map.values());
+  }
+
+  public static void main(String[] args) {
+    String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+    System.out.println(new Solution().groupAnagrams(strs));
+  }
 }
