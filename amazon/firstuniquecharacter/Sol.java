afefc28a5131489b914161518f0c001904b08501
prashant <prashant.kr.mod@gmail.com>
2021-04-20 23:54:44 -0400

    firstuniquecharacter-2

---
+++ amazon/firstuniquecharacter/Sol.java
@@ -0,0 +1,28 @@
+package firstuniquecharacter;
+
+public class Sol {
+    public static void main(String[] args) {
+        String s = "loveleetcode", s1 = "aabb";
+        System.out.println(new Sol().firstUniqChar(s));
+    }
+
+    public int firstUniqChar(String s) {
+        int[] count = new int[26];
+        int n = s.length();
+        // build char count bucket : <charIndex, count>
+        for (int i = 0; i < n; i++) {
+            int index = s.charAt(i) - 'a';
+            count[index]++;
+        }
+
+        // find the index
+        for (int i = 0; i < n; i++) {
+            int index = s.charAt(i) - 'a';
+            if (count[index] == 1) {
+                return i;
+            }
+
+        }
+        return -1;
+    }
+}
