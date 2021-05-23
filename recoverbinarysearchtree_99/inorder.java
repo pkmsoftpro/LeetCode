35f3e9edd84d18ecd98ae1f0ed0473c8a87753d2
prashant <prashant.kr.mod@gmail.com>
2021-05-23 17:38:38 -0400

    Update inorder.java

--- recoverbinarysearchtree_99/inorder.java
+++ recoverbinarysearchtree_99/inorder.java
@@ -5,6 +5,10 @@ package recoverbinarysearchtree_99;
  */
 
 /*
+(a) Inorder (Left, Root, Right) : 4 2 5 1 3 
+(b) Preorder (Root, Left, Right) : 1 2 4 5 3 
+(c) Postorder (Left, Right, Root) : 4 5 2 3 1
+
 	private void traverse (TreeNode_old root) {
 	   if (root == null)
 	      return;
