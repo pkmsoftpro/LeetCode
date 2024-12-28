9939605ad03f92c10eda96c173a4655677dee082
prashant <prashant.kr.mod@gmail.com>
2024-12-29 01:08:32 +0530

    Update EasySol.java

--- LongestString/EasySol.java
+++ LongestString/EasySol.java
@@ -5,6 +5,7 @@ import java.util.Set;
 //   https://www.youtube.com/watch?v=4-LshVFzfLI
 //https://www.youtube.com/watch?v=6avtxejCZYA
 //https://www.geeksforgeeks.org/length-of-the-longest-substring-without-repeating-characters/
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/solutions/3649636/3-method-s-c-java-python-beginner-friendly/
 
 
 public class EasySol {
